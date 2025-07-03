5afebfa9b960369d75486b296d819943aa72a7a5
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-07-03 12:52:33 +0200

    Skeleton code added

---
+++ public/PiSubmarine/Chipset/Api/PacketOut.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace PiSubmarine::Chipset::Api
+{
+	class PacketOut
+	{
+
+	};
+}
---
+++ test/PiSubmarine/Chipset/Api/PacketOutTest.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "PiSubmarine/Chipset/Api/PacketOut.h"
+
+namespace PiSubmarine::Chipset::Api
+{
+	TEST(PacketOutTest, Instantiation)
+	{
+
+	}
+}
