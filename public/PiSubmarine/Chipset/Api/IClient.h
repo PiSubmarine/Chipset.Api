506bddfaf27953887f2bf6e24e3122ddf7720b35
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-05 23:43:01 +0200

    Fixed types

--- public/PiSubmarine/Chipset/Api/IClient.h
+++ public/PiSubmarine/Chipset/Api/IClient.h
@@ -5,8 +5,8 @@
 #include <expected>
 #include <PiSubmarine/Celsius.h>
 #include <PiSubmarine/Volts.h>
-
-#include "Command.h"
+#include <PiSubmarine/Amperes.h>
+#include "PiSubmarine/Chipset/Api/Command.h"
 #include "PiSubmarine/NormalizedFraction.h"
 
 namespace PiSubmarine::Chipset
@@ -34,12 +34,12 @@ namespace PiSubmarine::Chipset
         virtual std::expected<Celsius, Status> GetChipsetTemperature() = 0;
         virtual std::expected<Celsius, Status> GetChargerTemperature() = 0;
 
-        virtual std::expected<Celsius, Status> GetChargerBusCurrent() = 0;
-        virtual std::expected<Celsius, Status> GetBatteryCurrent() = 0;
-        virtual std::expected<Celsius, Status> GetChargerBusVoltage() = 0;
-        virtual std::expected<Celsius, Status> GetBatteryVoltage() = 0;
-        virtual std::expected<Celsius, Status> GetChargerSystemVoltage() = 0;
-        virtual std::expected<Celsius, Status> GetBatteryTemperature() = 0;
+        virtual std::expected<Amperes, Status> GetChargerBusCurrent() = 0;
+        virtual std::expected<Amperes, Status> GetBatteryCurrent() = 0;
+        virtual std::expected<Volts, Status> GetChargerBusVoltage() = 0;
+        virtual std::expected<Volts, Status> GetBatteryVoltage() = 0;
+        virtual std::expected<Volts, Status> GetChargerSystemVoltage() = 0;
+        virtual std::expected<Volts, Status> GetBatteryTemperature() = 0;
 
         virtual Status SendCommand(Api::Command command) = 0;
 
