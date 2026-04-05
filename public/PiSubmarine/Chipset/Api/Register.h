6f448eae2a2e7685acf08e662aac2872e3d1e9a1
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-05 21:43:17 +0200

    ChipsetVdd added

--- public/PiSubmarine/Chipset/Api/Register.h
+++ public/PiSubmarine/Chipset/Api/Register.h
@@ -10,13 +10,14 @@ namespace PiSubmarine::Chipset::Api
     enum class Register
     {
         Status = 0x00,
-        RtcHigh = 0x01,
-        RtcLow = 0x02,
-        Reg5Voltage = 0x03,
-        RegPiVoltage = 0x04,
-        BallastPosition = 0x05,
-        ChipsetTemperature = 0x06,
-        ChargerTemperature = 0x07,
+        RtcHigh,
+        RtcLow,
+        ChipsetVdd,
+        Reg5Voltage,
+        RegPiVoltage,
+        BallastPosition,
+        ChipsetTemperature,
+        ChargerTemperature,
 
         Command
     };
