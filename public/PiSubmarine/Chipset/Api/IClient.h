c71c04e8abc1c25931c826afe669803eda758d36
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-16 15:06:46 +0200

    Error model updated

--- public/PiSubmarine/Chipset/Api/IClient.h
+++ public/PiSubmarine/Chipset/Api/IClient.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "PiSubmarine/Chipset/Api/Status.h"
-#include <chrono>
-#include <expected>
+#include <PiSubmarine/Amperes.h>
 #include <PiSubmarine/Celsius.h>
 #include <PiSubmarine/Volts.h>
-#include <PiSubmarine/Amperes.h>
+#include <chrono>
+
 #include "PiSubmarine/Chipset/Api/Command.h"
+#include "PiSubmarine/Chipset/Api/Status.h"
+#include "PiSubmarine/Error/Api/Result.h"
 #include "PiSubmarine/NormalizedFraction.h"
 
 namespace PiSubmarine::Chipset
@@ -14,34 +15,26 @@ namespace PiSubmarine::Chipset
     class IClient
     {
     public:
-        enum class Status
-        {
-            NoError,
-            ChipsetError,
-            ProtocolError
-        };
-
         virtual ~IClient() = default;
 
-        virtual std::expected<Api::Status, Status> GetStatus() const = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Api::Status> GetStatus() const = 0;
 
-        virtual std::expected<std::chrono::seconds, Status> GetRtc() = 0;
-        virtual Status SetRtc(const std::chrono::seconds& timestamp) = 0;
-        virtual std::expected<Volts, Status> GetChipsetVoltage() = 0;
-        virtual std::expected<Volts, Status> GetRegulator5Voltage() = 0;
-        virtual std::expected<Volts, Status> GetRegulatorPiVoltage() = 0;
-        virtual std::expected<NormalizedFraction, Status> GetBallastPosition() = 0;
-        virtual std::expected<Celsius, Status> GetChipsetTemperature() = 0;
-        virtual std::expected<Celsius, Status> GetChargerTemperature() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<std::chrono::seconds> GetRtc() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<void> SetRtc(const std::chrono::seconds& timestamp) = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetChipsetVoltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetRegulator5Voltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetRegulatorPiVoltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<NormalizedFraction> GetBallastPosition() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Celsius> GetChipsetTemperature() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Celsius> GetChargerTemperature() = 0;
 
-        virtual std::expected<Amperes, Status> GetChargerBusCurrent() = 0;
-        virtual std::expected<Amperes, Status> GetBatteryCurrent() = 0;
-        virtual std::expected<Volts, Status> GetChargerBusVoltage() = 0;
-        virtual std::expected<Volts, Status> GetBatteryVoltage() = 0;
-        virtual std::expected<Volts, Status> GetChargerSystemVoltage() = 0;
-        virtual std::expected<Celsius, Status> GetBatteryTemperature() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Amperes> GetChargerBusCurrent() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Amperes> GetBatteryCurrent() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetChargerBusVoltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetBatteryVoltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Volts> GetChargerSystemVoltage() = 0;
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<Celsius> GetBatteryTemperature() = 0;
 
-        virtual Status SendCommand(Api::Command command) = 0;
-
+        [[nodiscard]] virtual PiSubmarine::Error::Api::Result<void> SendCommand(Api::Command command) = 0;
     };
 }
